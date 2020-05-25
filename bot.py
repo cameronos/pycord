e82c31d801eb32517ef5c8f7dfd15489444916f1
cameron <34195086+vulture001@users.noreply.github.com>
2020-05-24 22:11:23 -0400

    Reformatted code
    done

--- bot.py
+++ bot.py
@@ -9,10 +9,17 @@ client = commands.Bot(command_prefix = '.') #Change prefix to your choice
 
 @client.event
 async def on_ready():
- print("Ready")
+    game = discord.Game("with code.. ")
+    await client.change_presence(activity=game)
+    print('Online! Succesfully Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
 @client.command()
-async def ping(ctx)
-  await ctx.send("Pong!")
+async def ping(ctx):
+    pingspeed = f'{round(client.latency * 1000)}'
+    embed = discord.Embed(colour=discord.Colour(0x7ed321), description=":ping_pong: " + pingspeed + " ms")
+    await ctx.send(embed=embed)
 
 client.run(TOKEN)
